e94fa748cce240e8c692117345e8b75e90fa759d
Allana Lima <lima.marques10@aluno.ifce.edu.br>
2025-10-15 20:22:33 +0000

    alterei-gabi

--- climas.js
+++ climas.js
@@ -4,15 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     cards.forEach(card => {
         card.addEventListener("click", function() {
             this.classList.toggle("active");
-            const cardDetails = this.querySelector(".card-details");
-
-            if (this.classList.contains("active")) {
-                cardDetails.style.maxHeight = cardDetails.scrollHeight + "px";
-                cardDetails.style.padding = "20px 25px";
-            } else {
-                cardDetails.style.maxHeight = "0";
-                cardDetails.style.padding = "0 25px";
-            }
         });
     });
 });
+
